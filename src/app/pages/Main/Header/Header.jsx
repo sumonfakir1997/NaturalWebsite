9b16a487b00377e1c3d864345ab2a23358ce6f95
sumon <sumonfakir1997@gmail.com>
2023-11-12 09:54:55 -0800

    project created

---
+++ src/app/pages/Main/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Box,Text } from "@chakra-ui/react";
+
+export const Header = () => {
+    return (
+        <>
+        <Box>
+            yes i am real header 
+        </Box>
+        </>
+    )
+}
